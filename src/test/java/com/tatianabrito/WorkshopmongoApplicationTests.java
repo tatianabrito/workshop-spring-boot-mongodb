237d61c7b15f353dbd4a815f07c13390abebdc9f
tatiana brito <brito.tatiana@gmail.com>
2020-09-11 09:07:01 -0300

    first commit

---
+++ src/test/java/com/tatianabrito/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tatianabrito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
