a751e5d546b75e7416d8bf0143f6a7a4509d7b7a
tatiana brito <brito.tatiana@gmail.com>
2020-09-27 19:08:08 -0300

    Referenciando os posts do usuário

--- src/main/java/com/tatianabrito/domain/Post.java
+++ src/main/java/com/tatianabrito/domain/Post.java
@@ -6,6 +6,8 @@ import java.util.Date;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import com.tatianabrito.dto.AuthorDTO;
+
 @Document
 public class Post implements Serializable {
 
@@ -15,19 +17,18 @@ public class Post implements Serializable {
 	private Date date;
 	private String title;
 	private String body;
-	private User user;
+	private AuthorDTO author;
 	
 	public Post() {
-		
 	}
 
-	public Post(String id, Date date, String title, String body, User user) {
+	public Post(String id, Date date, String title, String body, AuthorDTO author) {
 		super();
 		this.id = id;
 		this.date = date;
 		this.title = title;
 		this.body = body;
-		this.user = user;
+		this.author = author;
 	}
 
 	public String getId() {
@@ -62,12 +63,12 @@ public class Post implements Serializable {
 		this.body = body;
 	}
 
-	public User getUser() {
-		return user;
+	public AuthorDTO getAuthor() {
+		return author;
 	}
 
-	public void setUser(User user) {
-		this.user = user;
+	public void setAuthor(AuthorDTO author) {
+		this.author = author;
 	}
 
 	@Override
