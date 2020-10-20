d534d06cd4cba20ff384e8c8eab66ce9dc9f46e8
tatiana brito <brito.tatiana@gmail.com>
2020-10-20 19:41:44 -0300

    Consulta simples com query methods

--- src/main/java/com/tatianabrito/resources/PostResource.java
+++ src/main/java/com/tatianabrito/resources/PostResource.java
@@ -1,13 +1,17 @@
 package com.tatianabrito.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tatianabrito.domain.Post;
+import com.tatianabrito.resources.util.URL;
 import com.tatianabrito.services.PostService;
 
 @RestController
@@ -16,7 +20,6 @@ public class PostResource {
 
 	@Autowired
 	private PostService service;
-
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Post> finById(@PathVariable String id) {
@@ -24,4 +27,10 @@ public class PostResource {
 		return ResponseEntity.ok().body(post);
 	}
 
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> finByTitle(@RequestParam(value="text", defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitleContainingIgnoreCase(text);
+		return ResponseEntity.ok().body(list);
+	}
 }
