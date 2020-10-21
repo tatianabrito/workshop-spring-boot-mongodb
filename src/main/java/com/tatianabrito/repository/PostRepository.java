ae8cdc651613ffa089a3df160380466286f79ea8
tatiana brito <brito.tatiana@gmail.com>
2020-10-21 20:37:17 -0300

    Consulta com vários critérios

--- src/main/java/com/tatianabrito/repository/PostRepository.java
+++ src/main/java/com/tatianabrito/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.tatianabrito.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -14,4 +15,7 @@ public interface PostRepository extends MongoRepository<Post, String>{
 	
 	@Query("{ 'title' : { $regex: ?0, $options: 'i' } }")
 	public List<Post> searchTitle(String text);
+	
+	@Query(" { $and: [ { $or: [ { 'title' : { $regex: ?0, $options: 'i' } }, { 'body' : { $regex: ?0, $options: 'i' } }, { 'comments.text' : { $regex: ?0, $options: 'i' } } ]  }, { date: { $gte: ?1}  }, { date: { $lte: ?2}} ] } ")
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
